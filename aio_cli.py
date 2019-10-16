148e6169cf8ccf8257d2d6d567bfe17a6be233ad
runelb <rune@jidrot.se>
2019-10-16 14:09:19 +0200

    simple client and server modules

---
+++ aio_cli.py
@@ -0,0 +1,39 @@
+import asyncio
+
+async def tcp_echo_client(message):
+    reader, writer = await asyncio.open_connection(
+        '127.0.0.1', 8888)
+
+    print(f'Send: {message!r}')
+    writer.write(message.encode())
+
+    data = await reader.read(100)
+    print(f'Received: {data.decode()!r}')
+
+    print('Close the connection')
+    writer.close()
+
+# msg = input('Send message to server\n')
+# asyncio.run(tcp_echo_client(msg))
+
+async def client():
+    reader, writer = await asyncio.open_connection(
+        '127.0.0.1', 8888)
+
+    while True:
+        command = input("Client waiting\n")
+
+        if command == "quit":
+            print('Close the connection')
+            writer.close()
+            break
+
+        print(f'Send: {command!r}')
+        writer.write(command.encode())
+
+    # response from server
+    data = await reader.read(100)
+    print(f'Received: {data.decode()!r}')
+
+
+asyncio.run(client())
