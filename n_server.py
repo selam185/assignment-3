0854f172b4f2c352a8a359a681403a8487ec215d
Selvi <gnpe19@student.bth.se>
2019-10-21 07:20:24 -0700

    updated with register and login function by passing arguments

--- n_server.py
+++ n_server.py
@@ -1,24 +1,11 @@
-# import socket
-# import sys
-
-# # Create a TCP/IP socket
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server_address = ('127.0.0.1', 8880)
-# sock.bind(server_address)
-# sock.listen(2)
-
-# while True:
-#     # Wait for a connection
-#     connection, client_address = sock.accept()
-#     buffer = connection.recv(64)
-#     if len(buffer) > 0:
-#         #print(buffer)
-#         print(bytes.decode(buffer))
-#         break
-
 import asyncio
 import os
 import time
+import signal
+import pickle
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
 
 
 def llist():
@@ -51,48 +38,101 @@ def createDir(dir_name):
     except FileExistsError:
         result = "Directory " + dir_name + " already exists"
         print(result)
+
     return result
 
-def changeDir():
-     
-    print("Current Working Directory " , os.getcwd())  
-    try:
-        # Change the current working Directory    
-        os.chdir("..")
-        print("Directory changed")
-    except OSError:
-        print("Can't change the Current Working Directory")
+def changeDir(name):
+    
     print("Current Working Directory " , os.getcwd())
-    temp = os.getcwd()
-    return temp
 
-def writefile():
+    try:
+        if name == '..':
+            os.chdir('..')
+            print("previous directory changed", os.getcwd())
+        else:
+            os.chdir(name)
+            print("inside directory changed", os.getcwd())
 
-    f = open('newfile.txt', 'w')
-    f.write("Hello World!!")
+        print("Current Working Directory " , os.getcwd())
+        result = os.getcwd()
+        return "Current working directory " + result
+    
+    except OSError:
+        print("Request denied")
+        return "Request denied"
+
+    
+
+def writefile(name, text):
+
+    f = open(name, 'w')
+    f.write(text)
+    #f.write("This is Assignment3 related to file handling.")
     f.close()
-    return f
-    # print("This is write file")
+    result = "File " + name + " created"
+    return result
+    
+def readfile(name):
 
-    # with open('newfile.txt', 'w') as writefile:
-    #     writefile.write('Hello world!!This is Selvi')
-    #     writefile.close()
-    #     wr_file = ("File created successfully")
-    #     return wr_file
+    try:
+        f = open(name, 'r')
+        for char in f:
+            result = (char[:100])
+            print(result)
+            f.close()
+            return "file reading:    " + result
+    except OSError:
+        print("Request denied")
+        return "Request denied"
 
-def readfile():
+def register(username, password):
+    try:
+        with open('reg.pickle', 'rb') as f:
+            users = pickle.load(f)
+            userlist = users      
+    except:
+        users = None
+        userlist = {}
 
-    f = open('newfile.txt', 'r')
-    for line in f:
-    #f.read()
-        print(line)
-        return line
+    
+    if username not in userlist.keys():
+        userlist[username] = password
+        pickle.dump( userlist, open( "reg.pickle", "wb" ) )
+        #user = pickle.load( open( "reg.pickle", "rb" ) )
+        print("Register successfully")
+        result = username + " registered Sucessfully"
+        os.mkdir(username)
+        result = "User registered Sucessfully and Directory " + username + " created"
+        return result
+    
+    else:  
+        print("the username is already exits. Please enter valid")
+        return "Username already exists" 
 
-    # with open('newfile.txt', 'r') as readfile:
-    #     for line in readfile:
-    #         #print(line)
-    #         return line
-    #     readfile.close()
+
+def login(username, password):
+    try:
+        with open('reg.pickle', 'rb') as f:
+            users = pickle.load(f)
+            userlist = users
+        
+    except:
+        users = None
+        userlist = {}
+
+    if username in userlist.keys():
+        if password in userlist.values():
+            print("login successfully")
+            result = username + " Login Sucessfully"
+            return result
+        else:
+            return "Incorrect password"
+
+    else:
+        while username not in userlist.keys():       
+            print(" Please enter valid Username")
+            return "Invalid username"
+
 
 async def handle_commands(reader, writer):
     data = await reader.read(10000)
@@ -102,41 +142,38 @@ async def handle_commands(reader, writer):
 
     split_message = message.split(' ')
 
+    if split_message[0] == 'regist':
+        #register(split_message[1], split_message[2])
+        writer.write(register(split_message[1], split_message[2]).encode())
+        await writer.drain()
+
+    if split_message[0] == 'log':
+        #login(split_message[1], split_message[2])
+        writer.write(login(split_message[1], split_message[2]).encode())
+        await writer.drain()
+
     if message == 'list':
         llist()
         writer.write(llist().encode())
         await writer.drain()
 
-    if message == 'readfile':
-        readfile()
-        writer.write(readfile().encode())
+    if split_message[0] == 'readfile':
+        readfile(split_message[1])
+        writer.write(readfile(split_message[1]).encode())
         await writer.drain()
 
-    if message == 'writefile':
-        writefile()
-        writer.write(writefile().encode())
+    if split_message[0] == 'writefile':
+        writefile(split_message[1], split_message[2])
+        writer.write(writefile(split_message[1], split_message[2]).encode(encoding='utf-8'))
+        await writer.drain()
+
+    if split_message[0] == 'change' and split_message[1] == 'directory':
+        writer.write(changeDir(split_message[2]).encode())
         await writer.drain()
         
-    if message == 'change directory':
-        #changeDir()
-        writer.write(changeDir().encode())
-        await writer.drain()
-
     if split_message[0] == 'create' and split_message[1] == 'directory':
         writer.write(createDir(split_message[2]).encode())
         await writer.drain()
-
-    # if message == 'create directory ':
-    #     #createDir()
-    #     writer.write(createDir().encode())
-    #     await writer.drain()
-
-    
-    # print(f"Send: {message!r}")
-    # writer.write(data)
-    # await writer.drain()
-    # print("Close the connection")
-    # writer.close()
 
 async def main():
     server = await asyncio.start_server(
@@ -149,6 +186,3 @@ async def main():
         await server.serve_forever()
 
 asyncio.run(main())
-#changeDir()
-#writefile()
-#readfile()
